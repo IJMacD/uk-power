dcbfb6e1ecf35fc0d5d85c435c1a148a5654a3a5
Iain MacDonald <IJMacD@gmail.com>
2024-04-20 16:45:59 +0800

    Fetch from API every minute

--- src/hooks/useGridWatch.js
+++ src/hooks/useGridWatch.js
@@ -5,6 +5,8 @@ const API_URL = process.env.NODE_ENV === "production" ? "/api" : 'https://uk-pow
 
 export const DEMAND_CODE = "MAINCALC";
 
+const UPDATE_FREQUENCY = 1 * 60 * 1000;
+
 /**
  * @typedef Source
  * @property {string} code
@@ -18,8 +20,8 @@ export const DEMAND_CODE = "MAINCALC";
  * @returns {[string,Source[]]}
  */
 export function useGridWatch() {
-    const [updated, setUpdated] = React.useState(null);
-    const [sources, setSources] = React.useState([]);
+    const [updated, setUpdated] = React.useState("");
+    const [sources, setSources] = React.useState(/** @type {Source[]} */([]));
 
     React.useEffect(() => {
         const f = () => fetch(API_URL).then(r => r.json()).then(async (d) => {
@@ -28,14 +30,15 @@ export function useGridWatch() {
 
             const q = new Query({ sources });
 
-            const results = await q.run("FROM sources SELECT code, name, value, frequency ORDER BY value DESC", { output: "objects" });
-            // const inputs = await q.run("FROM sources WHERE code != 'RENEW' AND code != 'CARBON' AND value > 0 SELECT code, name, value ORDER BY value DESC", { output: "objects" });
+            /** @type {Source[]} */
+            const results = /** @type {any[]} */(await q.run("FROM sources SELECT code, name, value, frequency ORDER BY value DESC", { output: "objects" }));
+
             setSources(results);
-            // setInputs(inputs);
+
             setUpdated(d.date);
         });
         f();
-        const id = setInterval(f, 5 * 60 * 1000);
+        const id = setInterval(f, UPDATE_FREQUENCY);
         return () => clearInterval(id);
     }, []);
 
